9f3f11b0502e52189ac7aaf7fcc06eee544bdcff
christina <chrstnaheadley@gmail.com>
2021-02-10 13:25:50 -0500

    updates home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,59 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Movies</h1>
+    <div v-for="movie in movies" v-bind:key="movie.id">
+      <h2>{{ movie.title }}</h2>
+      <p>{{ movie.plot }}</p>
+      <p>Directed by: {{ movie.director }}</p>
+      <p>{{ movie.year }}</p>
+      <br />
+    </div>
   </div>
 </template>
 
+<style></style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      movies: [],
+      actors: [],
+    };
+  },
+  created: function() {
+    this.indexMovies();
+  },
+  methods: {
+    indexMovies: function() {
+      axios.get("http://localhost:3000/api/movies").then(response => {
+        console.log(response.data);
+        this.movies = response.data;
+      });
+    },
+    createMovies: function() {
+      var params = {
+        director,
+        english,
+        genres,
+        id,
+        plot,
+        title,
+        year,
+      };
+      axios
+        .post("http://localhost:3000/api/movies".params)
+        .then(response => {
+          this.movie = response.data;
+          this.movies.push(response.data);
+        })
+        .catch(error => {
+          console.log(error.response.data.errors);
+        });
+    },
   },
 };
 </script>
